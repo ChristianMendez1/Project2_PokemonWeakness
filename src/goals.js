2b6e620abb1407e6476a56890f6f27d235de8b67
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-04-21 14:09:06 -0400

    comments in goals.js explaining project

---
+++ src/goals.js
@@ -0,0 +1,15 @@
+//homepage:
+//text box
+//collage images of all the types
+//text box searches the type
+//explanation: tells you weapknesses , strengths etc
+
+//type/:type :
+//aligned left side list
+//image of type
+//Strength, Weakness, No effect
+//images of strength, weakness, no effect
+
+//bonus
+//dual types
+//specific pokemon weakness
