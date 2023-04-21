ecc4cfd0bf14902f3a797b5d149f84f521407b0a
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-04-21 15:03:02 -0400

    working on map in home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,21 @@
-export default function Home() {
+import { typename } from "./Data"
+import { Link } from "react-router-dom";
 
+export default function Home() {
 //map for types
 
+const typenamemap = typename.map((stock) => {
+    const {type} = stock;
     return(
-        <div>
-            
-        </div>
+        <Link to={`/type/${type}`}>
+            <li id='stockname'>{name}</li>
+        </Link>
     )
+})
+return (
+    <div id='dashboard'>
+        {stocksymbolmap}
+    </div>
+)
+
 }
